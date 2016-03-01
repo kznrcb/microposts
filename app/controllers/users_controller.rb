2e7c86d2dea516c4f5a088a8fc2caf134e205f00
kznrcb <kazunorichiba+cloud9@gmail.com>
2016-03-01 13:26:54 +0000

    re sent for task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
   before_action :collect_user, only: [:edit, :update]
   
   def show
-   @user = User.find(params[:id])
   end
   
   def edit
-   @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Successfully edited user profile'
       redirect_to @user
@@ -37,14 +34,10 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    unless logged_in?
-      flash[:danger] = "Invalid request"
-      redirect_to root_path
-    end
+    @user = User.find(params[:id])
   end
   
   def collect_user
-    @user = User.find(params[:id])
     if @user != current_user || !logged_in? then
      redirect_to root_path flash[:danger] = "Invalid request" 
     end
