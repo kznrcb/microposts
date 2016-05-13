cf136c8e59f2566cbae2ae5727e818b5812413b9
kznrcb <kazunorichiba+cloud9@gmail.com>
2016-05-13 07:56:55 +0000

    upload to heroku

--- db/migrate/20160311180324_rename_user_id_id_column_to_likes.rb
+++
@@ -1,7 +0,0 @@
-class RenameUserIdIdColumnToLikes < ActiveRecord::Migration
-  def change
-    # [形式] rename_column(テーブル名, 変更前のカラム名, 変更後のカラム名)
-    rename_column :likes, :user_id_id, :user_id
-    rename_column :likes, :micropost_id_id, :micropost_id
-  end
-end
